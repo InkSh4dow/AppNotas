9fc296315fbe3d4f693640c4689b641b2813a433
InkSh4dow <197549204+InkSh4dow@users.noreply.github.com>
2025-07-25 17:46:12 -0500

    Ultimos arreglos

--- app/src/main/java/com/jry/notas/Font.kt
+++ app/src/main/java/com/jry/notas/Font.kt
@@ -6,6 +6,8 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.jry.notas.R
+
 
 // Este archivo define la familia de fuentes personalizada en toda la ui
 
